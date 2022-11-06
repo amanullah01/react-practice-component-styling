6d7480122c58d379fde329c84012c45e79c799af
Amanullah Aman <amancse.aiub@gmail.com>
2022-11-06 22:02:14 +0600

    clear the field after save

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -49,6 +49,8 @@ const CourseInput = (props) => {
       return;
     }
     props.onAddGoal(enteredValue);
+
+    setEnteredValue("");
   };
 
   return (
@@ -57,7 +59,11 @@ const CourseInput = (props) => {
         className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
